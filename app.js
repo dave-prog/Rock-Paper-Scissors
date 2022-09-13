8f7e3fd21f4cb507c1150564cfb5f529a5dd9452
David <davidolanrewaju538@gmail.com>
2022-09-13 22:24:55 +0100

    Rock Paper & Scissors code completed

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 let arr = ["rock", "paper", "scissors"];
-//let input = prompt("Enter one of the following: Rock, Paper or Scissors");
 
+//selects computer optio from arr randomly
 function getComputerChoice() {
-    let computerChoice = arr[Math.floor(Math.random() * arr.length)];
-    //Math.random gets a random number from 0-1 but in this 0-array's length
-    //Math.floor gets an absolute value of the index ranging from 0-arr.length-1
-    return computerChoice;
+	let computerChoice = arr[Math.floor(Math.random() * arr.length-1)];
+	//Math.random gets a random number from 0-1 but in this 0-array's length
+	//Math.floor gets an absolute value of the index ranging from 0-arr.length-1
+	return computerChoice;
 }
 
-function getPlayerChoice() {
-    let input = prompt("Enter one of the following: Rock, Paper or Scissors");
-    return input;
-}
+//repeats the game
+for (let i = 0; i < 5; i++) {
+    function game() {
+        let playerInput = prompt("Enter one of the following: Rock, Paper or Scissors"); //accepts user input
+        let computerSelection = getComputerChoice();
+        let playerSelection = playerInput.toLowerCase();
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-
-function play(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-        console.log('It\'s a Tie')
+        function play(computerSelection, playerSelection) {
+            if (computerSelection === playerSelection) {
+                result = console.log("It's a Tie!");
+            } else if (computerSelection === "rock" && playerSelection === "paper") {
+                result = console.log("You Win!");
+            } else if (
+                computerSelection === "scissors" &&
+                playerSelection === "rock"
+            ) {
+                result = console.log("You Win!");
+            } else if (
+                computerSelection === "paper" &&
+                playerSelection === "scissors"
+            ) {
+                result = console.log("You Win!");
+            } else {
+                result = console.log("You Lose!");
+            }
+            return result;
+        }
+        return play(computerSelection, playerSelection);
     }
-
+    console.log(game());
 }
-
-
-console.log(computerSelection, playerSelection);
