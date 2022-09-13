769c255f62e04731cedc8876366ec64b2539bcd9
David <davidolanrewaju538@gmail.com>
2022-09-13 22:41:23 +0100

    Structured my comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let arr = ["rock", "paper", "scissors"];
 
-//selects computer optio from arr randomly
+//This function selects computer's option from the above array randomly
 function getComputerChoice() {
 	let computerChoice = arr[Math.floor(Math.random() * arr.length)];
 	//Math.random gets a random number from 0-1 but in this 0-array's length
@@ -8,35 +8,35 @@ function getComputerChoice() {
 	return computerChoice;
 }
 
-//repeats the game
+//This for loop repeats the game 5 times
 for (let i = 0; i < 5; i++) {
     function game() {
         let playerInput = prompt("Enter one of the following: Rock, Paper or Scissors"); //accepts user input
         let computerSelection = getComputerChoice();
         let playerSelection = playerInput.toLowerCase();
 
+        //This function compares playerSelection and computerSelection for results
         function play(computerSelection, playerSelection) {
             console.log(computerSelection, playerSelection);
             if (computerSelection === playerSelection) {
                 result = console.log("It's a Tie!");
-            } else if (computerSelection === "rock" && playerSelection === "paper") {
+            }
+            else if (computerSelection === "rock" && playerSelection === "paper") {
                 result = console.log("You Win!");
-            } else if (
-                computerSelection === "scissors" &&
-                playerSelection === "rock"
-            ) {
+            }
+            else if (computerSelection === "scissors" && playerSelection === "rock") {
                 result = console.log("You Win!");
-            } else if (
-                computerSelection === "paper" &&
-                playerSelection === "scissors"
-            ) {
+            }
+            else if (computerSelection === "paper" && playerSelection === "scissors") {
                 result = console.log("You Win!");
-            } else {
+            }
+            else {
                 result = console.log("You Lose!");
             }
             return result;
         }
         return play(computerSelection, playerSelection);
     }
+
     console.log(game());
 }
