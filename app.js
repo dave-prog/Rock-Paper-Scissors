2b4e6e0c004e063388a75ee22a8f3ba9c106c3e1
David <davidolanrewaju538@gmail.com>
2022-09-25 20:46:36 +0100

    Dynamism added to popup using JS

--- app.js
+++ app.js
@@ -1,42 +1,77 @@
-let arr = ["rock", "paper", "scissors"];
+const popup = document.querySelector('.rules-btn')
+const close = document.querySelector('.close-icon')
 
-//This function selects computer's option from the above array randomly
-function getComputerChoice() {
-	let computerChoice = arr[Math.floor(Math.random() * arr.length)];
-	//Math.random gets a random number from 0-1 but in this 0-array's length
-	//Math.floor gets an absolute value of the index ranging from 0-arr.length-1
-	return computerChoice;
+close.addEventListener("click", closePopup);
+popup.addEventListener("click", popUp);
+
+function popUp() {
+    document.querySelector(".popup-container").style.visibility = "visible";
+}
+function closePopup() {
+    document.querySelector('.popup-container').style.visibility = "hidden";
 }
 
-//This for loop repeats the game 5 times
-for (let i = 0; i < 5; i++) {
-    function game() {
-        let playerInput = prompt("Enter one of the following: Rock, Paper or Scissors"); //accepts user input
-        let computerSelection = getComputerChoice();
-        let playerSelection = playerInput.toLowerCase();
 
-        //This function compares playerSelection and computerSelection for results
-        function play(computerSelection, playerSelection) {
-            console.log(computerSelection, playerSelection);
-            if (computerSelection === playerSelection) {
-                result = console.log("It's a Tie!");
-            }
-            else if (computerSelection === "rock" && playerSelection === "paper") {
-                result = console.log("You Win!");
-            }
-            else if (computerSelection === "scissors" && playerSelection === "rock") {
-                result = console.log("You Win!");
-            }
-            else if (computerSelection === "paper" && playerSelection === "scissors") {
-                result = console.log("You Win!");
-            }
-            else {
-                result = console.log("You Lose!");
-            }
-            return result;
-        }
-        return play(computerSelection, playerSelection);
-    }
 
-    console.log(game());
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = ["rock", "paper", "scissors"];
+
+// //This function selects computer's option from the above array randomly
+// function getComputerChoice() {
+// 	let computerChoice = arr[Math.floor(Math.random() * arr.length)];
+// 	//Math.random gets a random number from 0-1 but in this 0-array's length
+// 	//Math.floor gets an absolute value of the index ranging from 0-arr.length-1
+// 	return computerChoice;
+// }
+
+// //This for loop repeats the game 5 times
+// for (let i = 0; i < 5; i++) {
+//     function game() {
+//         let playerInput = prompt("Enter one of the following: Rock, Paper or Scissors"); //accepts user input
+//         let computerSelection = getComputerChoice();
+//         let playerSelection = playerInput.toLowerCase();
+
+//         //This function compares playerSelection and computerSelection for results
+//         function play(computerSelection, playerSelection) {
+//             console.log(computerSelection, playerSelection);
+//             if (computerSelection === playerSelection) {
+//                 result = console.log("It's a Tie!");
+//             }
+//             else if (computerSelection === "rock" && playerSelection === "paper") {
+//                 result = console.log("You Win!");
+//             }
+//             else if (computerSelection === "scissors" && playerSelection === "rock") {
+//                 result = console.log("You Win!");
+//             }
+//             else if (computerSelection === "paper" && playerSelection === "scissors") {
+//                 result = console.log("You Win!");
+//             }
+//             else {
+//                 result = console.log("You Lose!");
+//             }
+//             return result;
+//         }
+//         return play(computerSelection, playerSelection);
+//     }
+
+//     console.log(game());
+// }
